372f9ef8187043cbea1996b32d914829d5557232
Donna Amos <donnacamos.developer@gmail.com>
2019-03-05 03:23:26 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -70,7 +70,10 @@ end
 def won?(board)
   WIN_COMBINATIONS.any? do |combo|
     if position_taken?(combo[0]) && @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]]
-      
+      return 
+      combo
+    end
+  end 
 end
 
 def full?(board)
