b4f5489b49f85e6f4dc64ad327c83afb38e5dd69
Donna Amos <donnacamos.developer@gmail.com>
2019-03-05 01:57:20 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -23,10 +23,10 @@ def board
 end 
 
 def display_board(board)
-  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 end 
