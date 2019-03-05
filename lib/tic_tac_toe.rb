8913e900bf5ffd4a89447c5126fa8cf81f93f20a
Donna Amos <donnacamos.developer@gmail.com>
2019-03-05 02:12:27 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -42,16 +42,9 @@ def display_board
   user_input.to_i - 1
 end
 
-def move(board, index, current_player)
-  board[index] = current_player
-end
+def move
+  
+end 
 
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
-end
-
-def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
-end
 
 end 
